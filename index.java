eab7d68ccec2f718232a93834a76d5830dbf889f
crist138 <cristoballongares@gmail.com>
2022-07-08 15:06:48 -0500

    su

--- index.java
+++ index.java
@@ -7,30 +7,14 @@ import java.math.*;
 import java.util.regex.*;
 
 
-
-
 public class index{
 
     public static void main(String[] args) {
 Scanner in = new Scanner(System.in);
+int h,m,s;
+h = in.nextInt();
 
-long[] arr = new long[5];
-int i,j;
-long k=0,l=0;
 
-    for(i=0;i<arr.length;i++){
-        arr[i] = in.nextLong();
-
-    }
-    Arrays.sort(arr);
-    for(j=1;j<arr.length;j++){
-        k+=arr[j];
-    }
-
-    for(j=0;j<arr.length-1;j++){
-        l+=arr[j];
-    }
-System.out.println(l+" "+k);
 
     }
 }
