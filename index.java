b12a9e8cfc3aada8446344139001b110ffb7e6af
crist138 <cristoballongares@gmail.com>
2022-07-08 21:12:47 -0500

    Testing in index...

--- index.java
+++ index.java
@@ -11,10 +11,43 @@ public class index{
 
     public static void main(String[] args) {
 Scanner in = new Scanner(System.in);
-int h,m,s;
-h = in.nextInt();
+String time;
+int count=0,i=11,sum=0,j=11,res=0;
+System.out.print("Ingresa la hora formato HH:mm:ssPM/AM : ");
+time = in.next();
 
+String[] oh = time.split(":");
 
+int hour = Integer.parseInt(oh[0]);
+String la = String.valueOf(oh[2]);
+la = la.substring(0,0) + la.substring(2);
+System.out.println(la);
+System.out.println(hour);
+int n = hour;
+sum = hour+i;
+res = hour-j;
+switch(la){
+	case "PM":
+		while(i<=sum){
+		i++;
+	}
+	la = la.substring(0,0) + la.substring(2);
+
+	System.out.println(i+":"+oh[1]+":"+oh[2]);
+	break;
+
+	case "AM":
+			if(hour==12){
+				while(j>=res){
+					j--;
+				}
+				System.out.println(j+":"+oh[1]+":"+oh[2]);		
+			}
+
+			System.out.println(oh[0]+":"+oh[1]+":"+oh[2]);
+
+		break;
+}
 
     }
 }
