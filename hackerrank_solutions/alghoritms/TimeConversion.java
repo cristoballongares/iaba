c76ceda47a2b085a299a0028c4b54ad1c46891c8
crist138 <cristoballongares@gmail.com>
2022-07-09 09:17:02 -0500

    Posible solution Time Conversion...

--- hackerrank_solutions/alghoritms/TimeConversion.java
+++ hackerrank_solutions/alghoritms/TimeConversion.java
@@ -6,8 +6,23 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+	
 
 public class index{
+
+    public static String
+    removingUpperCaseCharacters(String la)
+    {
+ 
+        String regex = "[A-Z]";
+ 
+        Pattern pattern = Pattern.compile(regex);
+ 
+        Matcher matcher = pattern.matcher(la);
+ 
+        return matcher.replaceAll("");
+    }
+
 
     public static void main(String[] args) {
 Scanner in = new Scanner(System.in);
@@ -21,32 +36,29 @@ String[] oh = time.split(":");
 int hour = Integer.parseInt(oh[0]);
 String la = String.valueOf(oh[2]);
 la = la.substring(0,0) + la.substring(2);
-System.out.println(la);
-System.out.println(hour);
 int n = hour;
 sum = hour+i;
 res = hour-j;
+System.out.println(la);
 switch(la){
 	case "PM":
 		while(i<=sum){
 		i++;
 	}
-	la = la.substring(0,0) + la.substring(2);
-
-	System.out.println(i+":"+oh[1]+":"+oh[2]);
+	la = la.substring(0,2);
+	System.out.println(i+":"+oh[1]+":"+la);
 	break;
 
 	case "AM":
-			if(hour==12){
-				while(j>=res){
-					j--;
-				}
-				System.out.println(j+":"+oh[1]+":"+oh[2]);		
+		if(hour==12){
+			while(j>=res){
+				j--;
 			}
-
-			System.out.println(oh[0]+":"+oh[1]+":"+oh[2]);
-
-		break;
+			System.out.println(j+":"+oh[1]+":"+removingUpperCaseCharacters(la));
+			}else{				
+				System.out.println(oh[0]+":"+oh[1]+":"+la);
+			}
+				break;
 }
 
     }
