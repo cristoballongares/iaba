7419053e6c49aec73cb344c1875b76c5cc9b8c83
crist138 <cristoballongares@gmail.com>
2022-07-10 12:45:49 -0500

    Learning POO JAVA

--- index.java
+++ index.java
@@ -5,49 +5,21 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-
-
 public class index{
 
-    public static void main(String[] args) {
-Scanner in = new Scanner(System.in);
-String time;
-int count=0,i=11,sum=0,j=11,res=0;
-System.out.print("Ingresa la hora formato HH:mm:ssPM/AM : ");
-time = in.next();
-
-String[] oh = time.split(":");
-
-int hour = Integer.parseInt(oh[0]);
-String la = String.valueOf(oh[2]);
-la = la.substring(0,0) + la.substring(2);
-System.out.println(la);
-System.out.println(hour);
-int n = hour;
-sum = hour+i;
-res = hour-j;
-switch(la){
-	case "PM":
-		while(i<=sum){
-		i++;
-	}
-	la = la.substring(0,0) + la.substring(2);
-
-	System.out.println(i+":"+oh[1]+":"+oh[2]);
-	break;
-
-	case "AM":
-			if(hour==12){
-				while(j>=res){
-					j--;
-				}
-				System.out.println(j+":"+oh[1]+":"+oh[2]);		
-			}
-
-			System.out.println(oh[0]+":"+oh[1]+":"+oh[2]);
-
-		break;
+public static class Coche{
+ String color,marca;
+ int km;
 }
+		public static void main(String[] args) {
+			Coche coche1 = new Coche();
+			
+			coche1.color = "Blanco";
+			coche1.marca = "Nose";
+			coche1.km = 12;
 
-    }
+			System.out.println("Color "+coche1.color+"\nMarca: "+coche1.marca+"\nKM: "+coche1.km);
+
+    	
+	}
 }
