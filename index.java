d0641f5710ee08ebc540b83250c6b10efa68019c
crist138 <cristoballongares@gmail.com>
2022-07-10 22:03:02 -0500

    Testing...

--- index.java
+++ index.java
@@ -7,40 +7,66 @@ import java.math.*;
 import java.util.regex.*;
 public class index{
 
+public static void clearScreen() {  
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();  
+}  
+
 public static class Coche{
  String color,marca;
  int km;
 }
 		public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	Random numbers = new Random(); // Generamos numero random
-	int count=0,inputNumber=0;
-	int secretNumber = numbers.nextInt(100)+1; // Asigamos un valor aleatorio entre el 1 al 100
+Scanner in = new Scanner(System.in);
+int inputNumber,minNumber=0,maxNumber=0,aki;
+String option;
+boolean win = false,trampa = false;
 
-	System.out.println("Adinivinador 2.0 - Dispones de 10 intentos para adivinar.");
+System.out.println("Adinivinare cualquier numero que pienes... de cualquier rango ej (0-100),(50-100),etc");
+System.out.println("Cuando ponga cualquier numero deberas poner: ");
+System.out.println("< Si es menor al numero que coloque");
+System.out.println("> Si es mayor al numero que coloque");
+System.out.println("= Si es igual al numero que coloque");
+System.out.printf("Dime el rango menor: ");
+minNumber = in.nextInt();
+System.out.printf("Dime el rango mayor: ");
+maxNumber = in.nextInt();
 
-	while(inputNumber!=secretNumber&&count!=10){
+	for(int i=1;i<=20;i++){
+	inputNumber = ((maxNumber-minNumber) / 2) + minNumber;
+		System.out.printf("El numero es %d\n",inputNumber);
+		option=in.nextLine();
 
-		System.out.printf("Intento %d: ",count+1);
-		inputNumber = in.nextInt();
-			if(inputNumber>secretNumber){
-				System.out.printf("El numero a adivinar es mayor\n");
-			} else if(inputNumber<secretNumber){
-				System.out.printf("El numero a adivinar es menor\n");
-			} else if(inputNumber==secretNumber){
+		switch(option){
+			case "<":
+				maxNumber=inputNumber-1;
 				break;
-			}
-			count++;
+			case ">":
+				minNumber=inputNumber+1;
+				break;
+			case "=":
+				win = true;
+				break;
+		}
+
+		if((minNumber>maxNumber)|| (maxNumber<minNumber)){
+			trampa=true;
+			break;
+		} else if(win){
+			break;
+		}
+
 	}
 
+if(win){
+	System.out.println("GANEEE, ez gg bot ezzZZzz, comprate unas manos");
+} else if(trampa) {
+	System.out.println("TRAMPOSO");
 
-	if(count==10){
-		System.out.printf("PERDISTE, el numero era %d\n", secretNumber);
-	} else if(inputNumber==secretNumber){
-		System.out.printf("GANASTE!!\n");
-	} else {
+}
 
-	}
+
+
 
 	}
 }
