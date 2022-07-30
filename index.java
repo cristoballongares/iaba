bff875f860fdd2af049c8d6755c10c624e24c2be
crist138 <cristoballongares@gmail.com>
2022-07-30 18:10:55 -0500

    Testing MasterMind 1.5

--- index.java
+++ index.java
@@ -2,664 +2,137 @@ import java.util.Scanner;
 import java.util.Random;
 import javax.swing.JOptionPane;
 import java.math.*;
+import java.util.*;
 public class index{
 
 		public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Random ran = new Random();
-		// Iteradores and other things...
-		int i,j,k,n=0,o=0,option,turn=1;
-		int g=0;
-		// Arrays para guardar las "cartas"
-		int[] grupo1 = new int[7],
-			  grupo2 = new int[7],
-			  grupo3 = new int[7];
-		// Deck
-		int[] deck = new int[21];
+		// Array
+		char[] characters = new char[4];
+		String respuesta;
+		char[] compareR = new char[4];
+		// variables uso general
+		int i=0,j,k,l,m,b=0,r=0,a,c;
 
-		boolean win = false;
+		// Booleano para determinar si gano o pardio dksajdsd
+		Boolean win = false;
 
-		// Generamos numeros sin repetirse
-		int num;
-		for(i=0;i<21;i++){
-			num = ran.nextInt(21)+1;
+		characters[0] = 'b';
+		characters[1] = 'a';
+		characters[2] = 'b';
+		characters[3] = 'b';
 
-			j=0;
-			while(j<i){ // "algoritmo" para guardar los numeros sin que se repitan, del 1 al 21 
-				if(deck[j]==num){
-					j=0;
-					num = ran.nextInt(21)+1;
-				} else{
-					j++;
-				}
+		/*
+		while(i<4){
+		characters[i] = (char)(ran.nextInt(26)+'a');
+		i++;
+		}
+		*/		
+		for(char x:characters){
+			System.out.print(x);
+		}
+
+		m=1;
+		i=1;
+		l=0;
+		boolean case1,case2,case3 = false;
+		System.out.println();
+		System.out.println("Dispones de 10 intentos para adivinar la combinacion de palabras al azar!\n'B: x': Si la letra se encuentra en la mismsa posicion\n'R: x': Si la letra coincide pero no esta en la misma posicion");
+		while(i<11){
+			b=0; // puntaje
+			r=0; // puntaje
+			m=0;
+			l=0;
+			a=0;
+			c=0;
+			System.out.print("\nIntento "+i+"\nEscribe la combinacion que piensas que sea correcta >> [uhdl], [fkcy] ");
+			respuesta = in.nextLine();
+			System.out.println();
+			i++;
+			for(j=0;j<4;j++){
+				compareR[j] = respuesta.charAt(j);
+					if(compareR[j]==characters[j]){
+						b++;
+					}
 			}
-		deck[i] = num;
-		}
-
-		/* 
-		for(int x:deck){
-			System.out.print(x+" ");
-		}
-		*/
-
-
-		// Repartimos 7 cartas a cada grupo
-
-		int z=14,w=7;
-		j=0;
-		for(i=0;i<7;i++){
-			grupo1[i] = deck[i];
-		}
-
-		for(i=0;i<7;i++){
-			grupo2[i]= deck[w];
-			w++;
-		}
-
-		for(i=0;i<7;i++){
-			grupo3[i]=deck[z];
-			z++;
-		}
-		z=0;
-		o=7;
-		n=14;
-		w=0;
-		while(win==false){
-				System.out.println();
-				System.out.println();
-				for(i=0;i<7;i++){
-					System.out.print(deck[z]+"\t"+deck[o]+"\t"+deck[n]+"\n");
-					z++;
-					o++;
-					n++;
+			for(k=0;k<4;k++){
+				if(compareR[0]==characters[k] && compareR[0]==characters[j-1]){
+					if(k==0){
+					} else{
+						r++;
+						m++;
+					}
+					System.out.println("0: "+r);
+				} 
+				if(compareR[1]==characters[k] && compareR[1]==characters[j-1]){
+					if(k==1){
+					} else{
+						r++;
+						l++;
+					}
+					System.out.println("1: "+r);
+				} 
+				if(compareR[2]==characters[k] && compareR[2]==characters[j-1]){
+					if(k==2){
+					} else{
+						r++;
+						a++;
+					}
+					System.out.println("2: "+r);
+				} 
+				if(compareR[3]==characters[k] && compareR[3]==characters[j-1]){
+					if(k==3){
+						c++;
+					} else {
+						r++;
+						c++;
+					}
+					System.out.println("3: "+r);
 				}
-			System.out.println();
-			System.out.println();
-			System.out.print("En que grupo se encuentra tu carta >> ");
-			option = in.nextInt();
-			turn++;
-			z=0;
-			o=7;
-			n=14;
-				switch(option){
-					case 1: // Hacemos la misma mamada de repartir ptm que desesperacion ayuda porfavor
-						for(i=0;i<7;i++){ 
-							deck[o] = grupo1[i];
-							deck[z] = grupo3[i];
-							deck[n] = grupo2[i];
-							o++;
-							z++;
-							n++;
-							}
-							
-							z=0;
-							o=7;
-							n=14;
-							g=0;
-							j=1;
-							w=2;
-
-									for(i=0;i<7;i++){
-										grupo1[i] = deck[g];
-										g+=3;
-									}
-
-									for(i=0;i<7;i++){
-										grupo2[i]= deck[j];
-										j+=3;
-									}
-
-									for(i=0;i<7;i++){
-										grupo3[i]=deck[w];
-										w+=3;
-									}
-							System.out.println();
-							System.out.println();
-
-								System.out.println();
-
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-							z=0;
-							o=7;
-							n=14;
-							switch(option){
-							case 1:
-								z=0;
-								o=7;
-								n=14;
-									for(i=0;i<7;i++){ 
-									deck[o] = grupo1[i];
-									deck[z] = grupo3[i];
-									deck[n] = grupo2[i];
-									o++;
-									z++;
-									n++;
-										}
-											z=0;
-											o=7;
-											n=14;
-											g=0;
-											j=1;
-											w=2;
-							System.out.println();
-
-								System.out.println();
-
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-								if(option==1){
-									System.out.print("\nLa carta es: "+deck[9]);
-								}else if(option==2){
-									System.out.print("\nLa carta es: "+deck[10]);
-								}else if(option==3){
-									System.out.print("\nLa carta es: "+deck[11]);
-								}
-								break;
-
-								case 2:
-								z=0;
-								o=7;
-								n=14;
-									for(i=0;i<7;i++){ 
-									deck[o] = grupo2[i];
-									deck[z] = grupo3[i];
-									deck[n] = grupo1[i];
-									o++;
-									z++;
-									n++;
-										}
-											z=0;
-											o=7;
-											n=14;
-											g=0;
-											j=1;
-											w=2;
-							System.out.println();
-							System.out.println();
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-								if(option==1){
-									System.out.print("\nLa carta es: "+deck[9]);
-								}else if(option==2){
-									System.out.print("\nLa carta es: "+deck[10]);
-								}else if(option==3){
-									System.out.print("\nLa carta es: "+deck[11]);
-								}
-
-								break;
-							
-
-							case 3:
-												z=0;
-												o=7;
-												n=14;
-													for(i=0;i<7;i++){ 
-													deck[o] = grupo3[i];
-													deck[z] = grupo2[i];
-													deck[n] = grupo1[i];
-													o++;
-													z++;
-													n++;
-														}
-															z=0;
-															o=7;
-															n=14;
-															g=0;
-															j=1;
-															w=2;
-											System.out.println();
-												for(i=1;i<22;i++){
-													System.out.print(deck[z]+"\t");
-													z++;
-													if(i%3==0){
-														System.out.println();
-													}
-												}
-
-													System.out.println();
-													System.out.println();
-
-
-											System.out.print("\nEn que grupo se encuentra tu carta >> ");
-											option = in.nextInt();
-												if(option==1){
-													System.out.print("\nLa carta es: "+deck[9]);
-												}else if(option==2){
-													System.out.print("\nLa carta es: "+deck[10]);
-												}else if(option==3){
-													System.out.print("\nLa carta es: "+deck[11]);
-												}
-
-												break;
-											}
-								break;
-					case 2: // help please
-						for(i=0;i<7;i++){ 
-							deck[o] = grupo2[i];
-							deck[z] = grupo3[i];
-							deck[n] = grupo1[i];
-							o++;
-							z++;
-							n++;
-							}
-							
-							z=0;
-							o=7;
-							n=14;
-							g=0;
-							j=1;
-							w=2;
-
-									for(i=0;i<7;i++){
-										grupo1[i] = deck[g];
-										g+=3;
-									}
-
-									for(i=0;i<7;i++){
-										grupo2[i]= deck[j];
-										j+=3;
-									}
-
-									for(i=0;i<7;i++){
-										grupo3[i]=deck[w];
-										w+=3;
-									}
-							System.out.println();
-							System.out.println();
-
-								System.out.println();
-
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-							z=0;
-							o=7;
-							n=14;
-							switch(option){
-							case 1:
-								z=0;
-								o=7;
-								n=14;
-									for(i=0;i<7;i++){ 
-									deck[o] = grupo1[i];
-									deck[z] = grupo3[i];
-									deck[n] = grupo2[i];
-									o++;
-									z++;
-									n++;
-										}
-											z=0;
-											o=7;
-											n=14;
-											g=0;
-											j=1;
-											w=2;
-							System.out.println();
-
-								System.out.println();
-
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-								if(option==1){
-									System.out.print("\nLa carta es: "+deck[9]);
-								}else if(option==2){
-									System.out.print("\nLa carta es: "+deck[10]);
-								}else if(option==3){
-									System.out.print("\nLa carta es: "+deck[11]);
-								}
-								break;
-
-								case 2:
-								z=0;
-								o=7;
-								n=14;
-									for(i=0;i<7;i++){ 
-									deck[o] = grupo2[i];
-									deck[z] = grupo3[i];
-									deck[n] = grupo1[i];
-									o++;
-									z++;
-									n++;
-										}
-											z=0;
-											o=7;
-											n=14;
-											g=0;
-											j=1;
-											w=2;
-							System.out.println();
-							System.out.println();
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-								if(option==1){
-									System.out.print("\nLa carta es: "+deck[9]);
-								}else if(option==2){
-									System.out.print("\nLa carta es: "+deck[10]);
-								}else if(option==3){
-									System.out.print("\nLa carta es: "+deck[11]);
-								}
-
-								break;
-							
-
-							case 3:
-												z=0;
-												o=7;
-												n=14;
-													for(i=0;i<7;i++){ 
-													deck[o] = grupo3[i];
-													deck[z] = grupo2[i];
-													deck[n] = grupo1[i];
-													o++;
-													z++;
-													n++;
-														}
-															z=0;
-															o=7;
-															n=14;
-															g=0;
-															j=1;
-															w=2;
-											System.out.println();
-												for(i=1;i<22;i++){
-													System.out.print(deck[z]+"\t");
-													z++;
-													if(i%3==0){
-														System.out.println();
-													}
-												}
-
-													System.out.println();
-													System.out.println();
-
-
-											System.out.print("\nEn que grupo se encuentra tu carta >> ");
-											option = in.nextInt();
-												if(option==1){
-													System.out.print("\nLa carta es: "+deck[9]);
-												}else if(option==2){
-													System.out.print("\nLa carta es: "+deck[10]);
-												}else if(option==3){
-													System.out.print("\nLa carta es: "+deck[11]);
-												}
-
-												break;
-											}
-										break;
-					case 3: // help please
-						for(i=0;i<7;i++){ 
-							deck[o] = grupo3[i];
-							deck[z] = grupo2[i];
-							deck[n] = grupo1[i];
-							o++;
-							z++;
-							n++;
-							}
-							
-							z=0;
-							o=7;
-							n=14;
-							g=0;
-							j=1;
-							w=2;
-
-									for(i=0;i<7;i++){
-										grupo1[i] = deck[g];
-										g+=3;
-									}
-
-									for(i=0;i<7;i++){
-										grupo2[i]= deck[j];
-										j+=3;
-									}
-
-									for(i=0;i<7;i++){
-										grupo3[i]=deck[w];
-										w+=3;
-									}
-							System.out.println();
-							System.out.println();
-
-								System.out.println();
-
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-							z=0;
-							o=7;
-							n=14;
-							switch(option){
-							case 1:
-								z=0;
-								o=7;
-								n=14;
-									for(i=0;i<7;i++){ 
-									deck[o] = grupo1[i];
-									deck[z] = grupo3[i];
-									deck[n] = grupo2[i];
-									o++;
-									z++;
-									n++;
-										}
-											z=0;
-											o=7;
-											n=14;
-											g=0;
-											j=1;
-											w=2;
-							System.out.println();
-							System.out.println();
-								System.out.println();
-
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-								if(option==1){
-									System.out.print("\nLa carta es: "+deck[9]);
-								}else if(option==2){
-									System.out.print("\nLa carta es: "+deck[10]);
-								}else if(option==3){
-									System.out.print("\nLa carta es: "+deck[11]);
-								}
-								break;
-
-								case 2:
-								z=0;
-								o=7;
-								n=14;
-									for(i=0;i<7;i++){ 
-									deck[o] = grupo2[i];
-									deck[z] = grupo3[i];
-									deck[n] = grupo1[i];
-									o++;
-									z++;
-									n++;
-										}
-											z=0;
-											o=7;
-											n=14;
-											g=0;
-											j=1;
-											w=2;
-							System.out.println();
-							System.out.println();
-								for(i=1;i<22;i++){
-									System.out.print(deck[z]+"\t");
-									z++;
-									if(i%3==0){
-										System.out.println();
-									}
-								}
-
-									System.out.println();
-									System.out.println();
-
-
-							System.out.print("\nEn que grupo se encuentra tu carta >> ");
-							option = in.nextInt();
-								if(option==1){
-									System.out.print("\nLa carta es: "+deck[9]);
-								}else if(option==2){
-									System.out.print("\nLa carta es: "+deck[10]);
-								}else if(option==3){
-									System.out.print("\nLa carta es: "+deck[11]);
-								}
-
-								break;
-							
-
-							case 3:
-												z=0;
-												o=7;
-												n=14;
-													for(i=0;i<7;i++){ 
-													deck[o] = grupo3[i];
-													deck[z] = grupo2[i];
-													deck[n] = grupo1[i];
-													o++;
-													z++;
-													n++;
-														}
-															z=0;
-															o=7;
-															n=14;
-															g=0;
-															j=1;
-															w=2;
-											System.out.println();
-												for(i=1;i<22;i++){
-													System.out.print(deck[z]+"\t");
-													z++;
-													if(i%3==0){
-														System.out.println();
-													}
-												}
-
-													System.out.println();
-													System.out.println();
-
-
-											System.out.print("\nEn que grupo se encuentra tu carta >> ");
-											option = in.nextInt();
-												if(option==1){
-													System.out.print("\nLa carta es: "+deck[9]);
-												}else if(option==2){
-													System.out.print("\nLa carta es: "+deck[10]);
-												}else if(option==3){
-													System.out.print("\nLa carta es: "+deck[11]);
-												}
-
-												break;
-											}
-										break;
-
-
-
-								
-				}
-				win = true;
+				
 			}
 
-		
+			/*
+			if(compareR[0]==characters[1] && compareR[0]!=characters[0]){
+				r++;
+			} 
+			if(compareR[0]==characters[2] && compareR[0]!=characters[1]){
+				r++;
+			}
+			if(compareR[0] == characters[3] && compareR[0]!=characters[2]){
+				r++;
+			}
+			if(compareR[1] == characters[0] && compareR[1]!=characters[1]){
+				r++;
+			} 
+			if(compareR[1] == characters[2] && compareR[1]!=characters[3]){
+				r++;
+			} 
+			if(compareR[1] == characters[3] && compareR[1]!=characters[2]){
+				r++;
+			}
+			if(compareR[2] == characters[3] && compareR[2]!=characters[2]){
+				r++;
+			}
+			if(compareR[2] == characters[1] && compareR[2]!=characters[0]){
+				r++;
+			}
+			if(compareR[2] == characters[0] && compareR[2]!=characters[1]){
+				r++;
+			}
+			if(compareR[3] == characters[1] && compareR[3]!=characters[0]){
+				r++;
+			}
+			if(compareR[3] == characters[2] && compareR[3]!=characters[1]){
+				r++;
+			}
+			if(compareR[3] == characters[0] && compareR[3]!=characters[3]){
+				r++;
+			}
+			*/
+			System.out.print("\nB: "+b+"\nR: "+r);
 
-		System.out.println("");
-
-
+		}
 	}
-
-
 }
